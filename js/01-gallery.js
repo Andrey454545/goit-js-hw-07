8720b89f51f7e50e3cfe29375011f3a9d3dcb0bb
Andrey454545 <109917137+Andrey454545@users.noreply.github.com>
2023-02-16 01:25:20 +0200

    update index.html

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,72 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// . по класу
+const gallery = document.querySelector('.gallery');
+
+const items = [];
+
+galleryItems.forEach(element) => {
+
+    const galleryItems = document.createElement('div');
+    galleryItems.classList.add('gallery__items');
+
+
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+
+    galleryLink.href = element.original;
+
+    // створення елемента з імям img повертає ссилку 
+    const galleryImg = document.createElement('img');
+    galleryImg.classList.add('gallery__image'); // додавання класа до тега
+
+    galleryImg.src = element.preview;
+
+    galleryImg.setAttribute('data-source', element.original);
+
+    galleryImg.alt = element.description;
+
+    galleryItem.append(galleryLink);
+    galleryLink.append(galleryImg);
+    items.push(galleryItem);
+};
+    // додавання створених елементів в галерею через розпилення
+gallery.append(...items);
+
+document.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    if (e.target.nodeName !== "IMG") {
+        return;
+    }
+
+
+    const imgSelected = e.target.getAttribute('data-source');
+
+    const template = basicLightbox.create (
+        `<img src = "${imgSelected}" width = "800" height = "600">`,
+    
+        // обьект налаштування бібліотеки
+    {
+    onShow: () => {         // показати
+        document.addEventListener("keydown", closeModal);
+    },
+        // заррити      
+    onClose: () => {
+        document.removeEventListener("keydown", closeModal);
+    },
+    }
+);
+
+template.show();
+
+function closeModal(e) {
+    if (e.key === "Escape") {
+        template.close();
+    }
+}
+});
+
+
+// console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(galleryItems);
